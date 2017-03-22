955f0176bd1155e9e0743d2f49e9c38dcc74cbff
Kent Godfrey <kenny_g1996@hotmail.co.uk>
2017-03-22 13:24:10 +0000

    Made canvas dimensions dynamic. Canvas now scales LIVE with browser dimensions.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,18 @@ var c = $("#mainCanvas");
 var ctx = c.get(0).getContext("2d");
 //Sets width of canvas to the width&height of browser window. get(0). Only works with CSS reset.
 c.attr("width", $(window).get(0).innerWidth); c.attr("height", $(window).get(0).innerHeight);
-//testing
-ctx.fillRect(0, 0, c.width(), c.height());
+
+
+//Uses JQuery Resize function to allow the canvas dimensions to be dynamically changed and updated.
+$(window).resize(resizeCanvas);
+	function resizeCanvas() {
+		c.attr("width", $(window).get(0).innerWidth); 
+		c.attr("height", $(window).get(0).innerHeight); 
+		//Placeholder rectangle. 
+		ctx.fillRect(0, 0, c.width(), c.height());
+	}; 
+
+resizeCanvas();
+
 
 
