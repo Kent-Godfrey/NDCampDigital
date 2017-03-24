bfa995ff4e508a61f52529d4ac9e0a19a87619e1
davidwarrington <davidmwarrington+github@gmail.com>
2017-03-24 17:44:13 +0000

    Added escape for projectile motion

--- js/projectile.js
+++ js/projectile.js
@@ -4,16 +4,16 @@ var c = $("#mainCanvas");
 //var ctx = c.getContext.("2d");
 var ctx = c.get(0).getContext("2d");
 //Sets width of canvas to the width&height of browser window. get(0). Only works with CSS reset.
-c.attr("width", $(window).get(0).innerWidth); c.attr("height", $(window).get(0).innerHeight);
-//Uses JQuery Resize function to allow the canvas dimensions to be dynamically changed and updated.
-$(window).resize(resizeCanvas);
-  function resizeCanvas() {
-    c.attr("width", $(window).get(0).innerWidth);
-    c.attr("height", $(window).get(0).innerHeight);
-    //Placeholder rectangle.
-    ctx.fillRect(0, 0, c.width(), c.height());
-  }
-resizeCanvas();
+// c.attr("width", $(window).get(0).innerWidth); c.attr("height", $(window).get(0).innerHeight);
+// //Uses JQuery Resize function to allow the canvas dimensions to be dynamically changed and updated.
+// $(window).resize(resizeCanvas);
+//   function resizeCanvas() {
+//     c.attr("width", $(window).get(0).innerWidth);
+//     c.attr("height", $(window).get(0).innerHeight);
+//     //Placeholder rectangle.
+//     ctx.fillRect(0, 0, c.width(), c.height());
+//   }
+// resizeCanvas();
 
 // NOTE Resizing the window causes canvas to clear unless functions are inside the resize function
 var x = c.width();
@@ -48,11 +48,19 @@ function drawCircle () {
 	ctx.arc(centreX, centreY, radius, 0, 2 * Math.PI);
 	ctx.closePath();
 	ctx.fill();
+
+  if (centreY === y) {
+    stopBoulder();
+  }
 }
 
 function getProjectileY(x) {
 	return Math.pow(x, 2);
 }
 
-setInterval(drawCircle, 33);
+function stopBoulder () {
+  clearInterval(fireBoulder);
+}
+
+var fireBoulder = setInterval(drawCircle, 33);
 });
