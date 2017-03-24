1b82d42be31f58462d7f9665b4553d1b8d43ac46
Ollie Crook <OllieCrook@gmail.com>
2017-03-24 15:36:28 +0000

    Got projectile and horde to work in tandem

--- js/projectile.js
+++ js/projectile.js
@@ -1,3 +1,19 @@
+$(document).ready(function(){
+//var c = document.getElementById("mainCanvas");
+var c = $("#mainCanvas");
+//var ctx = c.getContext.("2d");
+var ctx = c.get(0).getContext("2d");
+//Sets width of canvas to the width&height of browser window. get(0). Only works with CSS reset.
+c.attr("width", $(window).get(0).innerWidth); c.attr("height", $(window).get(0).innerHeight);
+//Uses JQuery Resize function to allow the canvas dimensions to be dynamically changed and updated.
+$(window).resize(resizeCanvas);
+  function resizeCanvas() {
+    c.attr("width", $(window).get(0).innerWidth);
+    c.attr("height", $(window).get(0).innerHeight);
+    //Placeholder rectangle.
+    ctx.fillRect(0, 0, c.width(), c.height());
+  }
+resizeCanvas();
 
 // NOTE Resizing the window causes canvas to clear unless functions are inside the resize function
 var x = c.width();
@@ -39,3 +55,4 @@ function getProjectileY(x) {
 }
 
 setInterval(drawCircle, 33);
+});
