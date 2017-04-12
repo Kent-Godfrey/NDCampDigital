4d4f95cba39c696f4e04071af5f530636410c8c7
ben whalley <ben247@gmail.com>
2017-04-12 12:46:24 +0100

    Amended tower.js

--- js/tower.js
+++ js/tower.js
@@ -1,9 +1,9 @@
-var canvas = document.getElementById('mainCanvas');
-var ctx = canvas.getContext('2d');
+var canvas = document.getElementById("mainCanvas");
+var ctx = canvas.getContext("2d");
 var imageObj = new Image();
 
 imageObj.onload = function() {
-  ctx.drawImage(imageObj, 0, 50);
+  ctx.drawImage(imageObj, 0, 100);
 };
 
-imageObj.src = 'images/tower.png';
+imageObj.src = "images/tower.png";
