14c858fb16459f81b60371e2259cc7ef4768728f
Kent Godfrey <kenny_g1996@hotmail.co.uk>
2017-04-15 14:51:40 +0100

    Animated skeleton set to walk towards Castle.Speed is easliy controlled.

--- js/horde.js
+++ js/horde.js
@@ -3,13 +3,14 @@ $(document).ready(function(){
 var c = $("#mainCanvas");
 //var ctx = c.getContext.("2d");
 var ctx = c.get(0).getContext("2d");
+ctx.imageSmoothingEnabled= false;
 //Sets width of canvas to the width&height of browser window. get(0). Only works with CSS reset.
 c.attr("width", $(window).get(0).innerWidth); c.attr("height", $(window).get(0).innerHeight);
 //Uses JQuery Resize function to allow the canvas dimensions to be dynamically changed and updated.
 $(window).resize(resizeCanvas);
 	function resizeCanvas() {
 		c.attr("width", $('.canvas-container').width());
-      c.attr("height", $('.canvas-container').height());
+     	c.attr("height", $('.canvas-container').height());
 		//Placeholder rectangle.
 		ctx.fillRect(0, 0, c.width(), c.height());
 	};
@@ -18,12 +19,25 @@ resizeCanvas();
 //Vars created to easily access the canvas W & H
 var canvasWidth = c.width();
 var canvasHeight= c.height();
+var skele = new Image();
+skele.src = 'images/skeletonAnimation.png';
+skele.addEventListener("load", loadImage, false);
+
+function loadImage(e){
+	animate();
+}
 
 //horde properties 
 var horde = {
 	"sqrx":0,
 	"rectSize":50,
 	"startPos":canvasWidth,
+	"shift":0,
+	"frameWidth":32.1,
+	"frameHeight":47,
+	"totalFrames":9,
+	"currentFrame":0,
+
 }
 
 //Animation
@@ -34,16 +48,28 @@ function animate(){
 	//Clears canvas clean
 	ctx.clearRect(0,0,canvasWidth, canvasHeight);
 	//Draws Square in new Position
-	ctx.fillRect(canvasWidth- horde.sqrx,690,horde.rectSize,horde.rectSize);
+	//ctx.fillRect(canvasWidth- horde.sqrx,690,horde.rectSize,horde.rectSize);
+	ctx.drawImage(skele, horde.shift,0,horde.frameWidth,horde.frameHeight,sqrActPos,690,horde.frameWidth,horde.frameHeight);
+	
+	horde.shift+= horde.frameWidth +1;
+
+	if (horde.currentFrame == horde.totalFrames){
+
+		horde.shift = 0;
+		horde.currentFrame= 0;
+	}
+
+	horde.currentFrame++;
+	//SPEAK TO THE TEAM ABOUT THIS.
+	//requestAnimationFrame(animate);
+
 	//Controlls speed
 	setTimeout(animate,120);
 	canvasWidth - horde.sqrx;
 	//console.log(horde.sqrx);
 	console.log(horde.startPos);
 	console.log(sqrActPos);
-	if(sqrActPos = 0){
-
-	}
+	
 };
 animate();
 
