a40213f00124ae53ede5289697b068ab8c1dc596
Ollie Crook <OllieCrook@gmail.com>
2017-05-03 13:46:22 +0100

    Deleted redundant files

--- js/tower.js
+++
@@ -1,9 +0,0 @@
-var canvas = document.getElementById("mainCanvas");
-var ctx = canvas.getContext("2d");
-var imageObj = new Image();
-
-imageObj.onload = function() {
-  ctx.drawImage(imageObj, 0, 100);
-};
-
-imageObj.src = "images/tower.png";
