181314104d7b2992ef3e59e5b18a42ddf09da9c5
Ollie Crook <OllieCrook@gmail.com>
2017-05-04 23:58:38 +0100

    Customised Twitter feed and fixed python

--- stream.py
+++ stream.py
@@ -2,15 +2,16 @@
 from twython import TwythonStreamer
 
 # Set API keys and Authorisation Keys
-APP_KEY = 'kixqDAGpTOlm47RwkMkmEAHH1'
-APP_SECRET = 'DFg6KWgTKsA28SKolpKGtq8rkbfJO3p0iZ8i8Gao8CQ0k269Ti'
-OAUTH_TOKEN = '1955200878-2UrPIyhPrRHi54iXJWvvZ02zPbxTmBgKJbAWxIl'
-OAUTH_TOKEN_SECRET = 'RrDTkkcV3uIFFu7PEc5pndeXKAqoo9o7QDT77W7BgUfKW'
+APP_KEY = 'KqhuVMbOmSPcmsk5hyg10GhIb'
+APP_SECRET = 'leeG4zajwKnCWskVJOuZoxgHAMqNJNLi6wVLc3VEthK0KMIBmd'
+OAUTH_TOKEN = '1955200878-95tQE1kshN62pelgTAbILZgFKXz7cKnvBo1Eg1U'
+OAUTH_TOKEN_SECRET = 'RWgGjSyZgCESJsfvopmrYBvMM89jgfydCyOWjoxBGkKPk'
 
 
 class MyStreamer(TwythonStreamer):
 
-    # On success print text from the data, text is the body of the tweet which will contain the keyword
+    # On success print text from the data,
+    # text is the body of the tweet which will contain the keyword
     def on_success(self, data):
         if 'text' in data:
             print(data['text'].encode('utf-8'))
@@ -19,13 +20,15 @@ class MyStreamer(TwythonStreamer):
     def on_error(self, status_code, data):
         print(status_code)
 
-        self.disconnect()
+        # self.disconnect()
+
 
 # Pipe API information into the streamer
 stream = MyStreamer(APP_KEY, APP_SECRET,
                     OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-# Track keywords seperated by commas and look for the most recent tweets (other arg is popular)
+# Track keywords seperated by commas and
+# look for the most recent tweets (other arg is popular)
 stream.statuses.filter(
-    track='@WeAreSigma, #CampDigital, "Camp Digital", #NorthernDabblers',
+    track='@WeAreSigma, #CampDigital, "Camp Digital", bbc',
     result_type='recent')
